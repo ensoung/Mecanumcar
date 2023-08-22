2f8631cd803669809b8749ada040f738932b96ce
Aiden <aiden.es.you@gmail.com>
2023-08-21 20:56:28 -0600

    made more comments and made the code more simple and more streamlined

--- mcnamum_wheel/mcnamum_wheel.ino
+++ mcnamum_wheel/mcnamum_wheel.ino
@@ -56,7 +56,7 @@ void loop() {
     }else if(l_trig && r_trig){  // R-L Trigger both pressed mean breaking 0~1023 0~0x3ff
       int breaking;
       if(car_status==FORWARD){  //depending on the direction the car is headed then the opposite trigger will be used as a break
-        breaking = l_trig;
+        breaking = l_trig;                        
       }else if(car_status=BACKWARD){
         breaking = r_trig;
       }else
